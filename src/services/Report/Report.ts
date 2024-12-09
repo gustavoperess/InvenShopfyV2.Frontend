38c4686f1e8ce5c26808c6e66142702de0248a01
Gustavo Moreira <gustavoluisperesmoreira@gmail.com>
2024-12-09 11:01:12 +0000

    removing url

--- src/services/Report/Report.ts
+++ src/services/Report/Report.ts
@@ -1,7 +1,7 @@
 import { DateRange } from '@mui/icons-material';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const BACKEND_URL =   "https://invenshopfy-backend-gqfwethzeggegqdv.westeurope-01.azurewebsites.net";
+const BACKEND_URL =   process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export const reportsApi = createApi({
     reducerPath: 'reportsApi',
